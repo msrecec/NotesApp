788cfac9b08f0c02c042b1c2b878ec5fb86f08d8
msrecec <67606889+msrecec@users.noreply.github.com>
2021-08-11 14:34:17 +0200

    First Challenge Solved

--- app.js
+++ app.js
@@ -4,4 +4,10 @@ const args = 'My name is Mislav';
 
 fs.writeFileSync('notes.txt', args);
 
-console.log(`Logging this argument write to file - ${args}`);
+console.log(`Logging this argument write to file - ${fs.readFileSync('notes.txt')}`);
+
+const appendingArgs = ' and this is awesome';
+
+fs.appendFileSync('notes.txt', appendingArgs)
+
+console.log(`Logging this argument write to file - ${fs.readFileSync('notes.txt')}`);
