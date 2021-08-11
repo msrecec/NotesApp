975c27a5482df76b6ab778aad6187297e7c7a515
msrecec <67606889+msrecec@users.noreply.github.com>
2021-08-11 14:53:28 +0200

    added more exports

--- app.js
+++ app.js
@@ -1,13 +1,7 @@
-const fs = require('fs');
+// const name = require('./utils.js')
+const add = require('./utils.js')
 
-const args = 'My name is Mislav';
+const sum = add(2,2);
 
-fs.writeFileSync('notes.txt', args);
-
-console.log(`Logging this argument write to file - ${fs.readFileSync('notes.txt')}`);
-
-const appendingArgs = ' and this is awesome';
-
-fs.appendFileSync('notes.txt', appendingArgs)
-
-console.log(`Logging this argument write to file - ${fs.readFileSync('notes.txt')}`);
+// console.log(name);
+console.log(sum);
