b8b201676c9baa7e97a60774818ee4adc87edd0e
msrecec <67606889+msrecec@users.noreply.github.com>
2021-08-14 15:40:49 +0200

    playing with some promises

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
 const validator = require('validator');
 const getNotes = require('./notes.js');
+const fs = require('fs');
+const util = require('util');
 
-const myNotes = getNotes();
-console.log(myNotes);
+async function myAsync(file = 'util.txt') {
+  const readFileAsync = util.promisify(fs.readFile);
 
-console.log(validator.isEmail('mislav@example.com'));
-console.log(validator.isURL('https://www.mislavexample.com'));
+  const buff = await readFileAsync(file);
+
+  if (buff) {
+    return buff.toString();
+  } else {
+    return `There is no text in ${file}`;
+  }
+}
+
+async function myApp() {
+  const myNotes = await getNotes();
+
+  console.log(`Contents of notes.txt are: ${myNotes}`);
+
+  const utilNotes = await myAsync('./util.txt');
+
+  console.log(`Contents of util.txt are: ${utilNotes}`);
+
+  console.log(validator.isEmail('mislav@example.com'));
+  console.log(validator.isURL('https://www.mislavexample.com'));
+}
+
+myApp();
--- notes.js
+++ notes.js
@@ -1,7 +1,14 @@
 const fs = require('fs');
+const util = require('util');
 
-function getNotes() {
-  return fs.readFileSync('./notes.txt').toString();
+async function getNotes() {
+  const readFileAsync = util.promisify(fs.readFile);
+  const text = await readFileAsync('./notes.txt');
+  if (text) {
+    return text.toString();
+  } else {
+    return 'notes.txt is empty';
+  }
 }
 
 module.exports = getNotes;
